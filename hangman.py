ce1abdfbaacdad5fbbd710fe9b237ca118dfcbac
sugandha27 <31508123+sugandha27@users.noreply.github.com>
2020-07-17 01:54:39 -0700

    updated words

--- hangman.py
+++ hangman.py
@@ -1,4 +1,11 @@
+import random
+
+def hangman():
+    word=random.choice(["kettle","table","chair","doraemon","pokemon","cricket","covid","lovely","great","kitchen","snake","joint","hyper","shinchan","ragnarok","alaska","penguin","typical"])
+    
+
 name=input("Enter your name\n")
 print("Welcome", name)
 print("-----------------------")
 print("Please try to guess the word in less than 10 attempts!")
+hangman()
